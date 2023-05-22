445c1a1d53e6ee1809305ab037ac229a1f2dcd23
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-05-22 17:52:23 -0400

    Clean & sort before building tree

--- src/balancedBST.js
+++ src/balancedBST.js
@@ -11,27 +11,26 @@ const Node = (value = null, left = null, right = null) => {
 };
 
 const buildTree = (array, start, end) => {
-  // Clean and sort the array
-  const cleanedArray = cleanArray(array);
-  const sortedArray = mergeSort(cleanedArray);
-  const uniqueSortedArray = [...new Set(sortedArray)];
-
   // Base case
   if (start > end) return null;
 
   // Set root node to middle element
-
+  const mid = (start + end) / 2;
+  const root = Node(array[mid]);
   // Build tree with right array and set to right
   // Same with left
   // Finally return the root node
 
-  if (uniqueSortedArray) return uniqueSortedArray;
-  return "no";
+  return root;
 };
 
 const Tree = (array) => {
   if (!Array.isArray(array)) return undefined;
-  const root = buildTree(array, 0, array.length - 1);
+  // Clean and sort the array
+  const cleanedArray = cleanArray(array);
+  const sortedArray = mergeSort(cleanedArray);
+  const uniqueSortedArray = [...new Set(sortedArray)];
+  const root = buildTree(uniqueSortedArray, 0, array.length - 1);
   return {
     get root() {
       return root;
