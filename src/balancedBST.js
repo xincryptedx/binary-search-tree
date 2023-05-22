38397aeacd96dd2305fa0cb0b760be76877dec23
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-05-22 17:48:14 -0400

    Add start and end params for buildTree

--- src/balancedBST.js
+++ src/balancedBST.js
@@ -10,21 +10,28 @@ const Node = (value = null, left = null, right = null) => {
   return newNode;
 };
 
-const buildTree = (array) => {
+const buildTree = (array, start, end) => {
+  // Clean and sort the array
   const cleanedArray = cleanArray(array);
   const sortedArray = mergeSort(cleanedArray);
   const uniqueSortedArray = [...new Set(sortedArray)];
+
+  // Base case
+  if (start > end) return null;
+
   // Set root node to middle element
+
   // Build tree with right array and set to right
   // Same with left
   // Finally return the root node
+
   if (uniqueSortedArray) return uniqueSortedArray;
   return "no";
 };
 
 const Tree = (array) => {
   if (!Array.isArray(array)) return undefined;
-  const root = buildTree(array);
+  const root = buildTree(array, 0, array.length - 1);
   return {
     get root() {
       return root;
