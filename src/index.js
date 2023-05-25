c9593718ce5c5940c4ba85b31bb4b7fe6c1e3b8b
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-05-25 18:13:57 -0400

    Test BST module in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 import BalancedBST from "./balancedBST";
 
 window.bbst = BalancedBST;
+
+// Testing Purposes Only
+function randomArray() {
+  const arr = [];
+  const maxValue = 1000;
+  for (let i = 0; i <= 35; i += 1) {
+    arr.push(Math.floor(Math.random() * maxValue));
+  }
+  return arr;
+}
+
+window.myBst = BalancedBST(randomArray());
+window.myBst.prettyPrint();
+console.log(`Binary search tree balanced? = ${window.myBst.isBalanced()}`);
+console.log(`Breadth-first order: `);
+console.log(window.myBst.levelOrder());
+console.log(`In-order: `);
+console.log(window.myBst.inOrder());
+console.log(`Pre-order: `);
+console.log(window.myBst.preOrder());
+console.log(`Post-order: `);
+console.log(window.myBst.postOrder());
+
+const newVals = randomArray();
+console.log(`Adding new values:`);
+console.log(newVals);
+newVals.forEach((val) => {
+  window.myBst.insert(val);
+});
+console.log(`New tree structure:`);
+window.myBst.prettyPrint();
+
+console.log(`Rebalancing tree... New balanced tree structure:`);
+window.myBst.rebalance();
+window.myBst.prettyPrint();
+
+console.log(`Binary search tree balanced? = ${window.myBst.isBalanced()}`);
+console.log(`Breadth-first order: `);
+console.log(window.myBst.levelOrder());
+console.log(`In-order: `);
+console.log(window.myBst.inOrder());
+console.log(`Pre-order: `);
+console.log(window.myBst.preOrder());
+console.log(`Post-order: `);
+console.log(window.myBst.postOrder());
